0783b6da88f70fb330bca8a54b74fdf5e1941797
Stephen F. Booth <me@sbooth.org>
2021-09-08 14:32:31 -0500

    Add midpoint test

--- Tests/GeodesicTests/GeodesicTests.swift
+++ Tests/GeodesicTests/GeodesicTests.swift
@@ -7,6 +7,16 @@ final class GeodesicTests: XCTestCase {
 		let lax = CLLocationCoordinate2D(latitude: 33.9424964, longitude: -118.4080486)
 		let jfk = CLLocationCoordinate2D(latitude: 40.6399278, longitude: -73.7786925)
 		let distance = lax.distanceTo(jfk)
-		XCTAssert(Int(distance) == 3982961)
+		XCTAssertEqual(distance, 3982961, accuracy: 1)
+	}
+
+	func testMidpoints() {
+		let lax = CLLocationCoordinate2D(latitude: 33.9424964, longitude: -118.4080486)
+		let jfk = CLLocationCoordinate2D(latitude: 40.6399278, longitude: -73.7786925)
+		let midpoint = lax.coordinate(atFractionOfDistance: 0.5, alongGeodesicTo: jfk)
+		let waypoints = lax.waypoints(count: 1, alongGeodesicTo: jfk)
+		XCTAssertEqual(waypoints.count, 1)
+		XCTAssertEqual(midpoint.latitude, waypoints.first!.latitude, accuracy: 0.0001)
+		XCTAssertEqual(midpoint.longitude, waypoints.first!.longitude, accuracy: 0.0001)
 	}
 }
