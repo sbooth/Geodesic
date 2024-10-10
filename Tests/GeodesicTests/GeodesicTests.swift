6e3e47c23a6b2ec16affed8a53506cb57a11b345
Stephen F. Booth <me@sbooth.org>
2024-10-10 10:55:15 -0500

    Attempt to resolve GitHub CI errors

--- Tests/GeodesicTests/GeodesicTests.swift
+++ Tests/GeodesicTests/GeodesicTests.swift
@@ -12,7 +12,7 @@ import CoreLocation
 	let lax = CLLocationCoordinate2D(latitude: 33.9424964, longitude: -118.4080486)
 	let jfk = CLLocationCoordinate2D(latitude: 40.6399278, longitude: -73.7786925)
 	let distance = lax.distanceTo(jfk)
-	#expect(abs(distance - 3982961) < 0.5)
+	#expect(Swift.abs(distance - 3982961) < 0.5)
 }
 
 @Test func testMidpoints() {
@@ -21,6 +21,6 @@ import CoreLocation
 	let midpoint = lax.coordinate(atFractionOfDistance: 0.5, alongGeodesicTo: jfk)
 	let waypoints = lax.waypoints(count: 1, alongGeodesicTo: jfk)
 	#expect(waypoints.count == 1)
-	#expect(abs(midpoint.latitude - waypoints.first!.latitude) < 0.001)
-	#expect(abs(midpoint.longitude - waypoints.first!.longitude) < 0.001)
+	#expect(Swift.abs(midpoint.latitude - waypoints.first!.latitude) < 0.001)
+	#expect(Swift.abs(midpoint.longitude - waypoints.first!.longitude) < 0.001)
 }
